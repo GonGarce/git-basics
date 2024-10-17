05fd1f7796dd11345c0342b07ec8bce1ba865fd6
Gon GArce <gonzalo.nom.es@gmail.com>
2024-10-17 13:16:20 +0200

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,43 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import starlightDocSearch from "@astrojs/starlight-docsearch";
+import { loadEnv } from "vite";
+
+const { ALGOLIA_API_KEY, ALGOLIA_APP_ID, ALGOLIA_INDEX_NAME } = loadEnv(
+  process.env.NODE_ENV ?? "",
+  process.cwd(),
+  ""
+);
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "Git Basics",
+      social: {
+        github: "https://github.com/gongarce/Git-Basics"
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", slug: "guides/example" }
+          ]
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" }
+        }
+      ],
+      plugins: [
+        starlightDocSearch({
+          appId: ALGOLIA_APP_ID,
+          apiKey: ALGOLIA_API_KEY, 
+          indexName: ALGOLIA_INDEX_NAME
+        })
+      ]
+    })
+  ]
 });
